ba6489476dcb00d9787e28c13cc51d448468626c
LacunaStrategies <102769956+LacunaStrategies@users.noreply.github.com>
2022-05-18 15:56:58 -0700

    Clean Up
    Replaced wallets with randomly generated addresses
    Updated to dynamic fetch based on "whitelist" or "team" type passed to API
    Generated test coupons

--- coupon-signer/pages/api/retrieveCoupons.js
+++ coupon-signer/pages/api/retrieveCoupons.js
@@ -1,39 +1,85 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { keccak256, toBuffer, ecsign, bufferToHex } from 'ethereumjs-util';
 import { ethers } from 'ethers';
+const fs = require('fs')
 
 export default function handler(req, res) {
   
-  const fs = require('fs')
-
-  // Private key generated from ethers.Wallet.createRandom()
-  // @notice The address used in your Smart Contract to verify the coupon must be the public address for private key  
+  let {type} = req.query
+ 
+  if (type === undefined){
+    res.status(400).json({message:'Error: Invalid Request - "type" is a required parameter'})
+    return
+  }
+  if (type !== "whitelist" && type !== "team"){
+    res.status(400).json({message:'Error: Invalid Request - valid "type" values are "whitelist" and "team"'})
+    return
+  }  
+  
+  // Prepare an empty coupons object to store our coupons
+  let coupons = {};
+  
+  /**
+   * * signerPvtKeyString
+   * Private key generated from ethers.Wallet.createRandom() - stored as a non-public environment variable
+   * @notice The address used in your Smart Contract to verify the coupon must be the public address associated with this key  
+   */
   const signerPvtKeyString = process.env.COUPON_SIGNING_KEY || "";
   const signerPvtKey = Buffer.from(signerPvtKeyString, "hex");
 
-  // The JSON object of whitelist addresses
-  // @notice The JSON structure should be: key = wallet address (string), value = allotted mints (int)
-  // @example:
-  // {"0x123456789...":2,"0x987654321...":10}
-  const whitelistFile = 'devwhitelist.json'; // should be an API req variable
-  const whitelistAddresses = require(`/lib/${whitelistFile}`);
+  /**
+   * * addressList
+   * The JSON objects of addresses and their associated allotted mints
+   * @notice The JSON structure should be: key = wallet address (string), value = allotted mints (int)
+   * @example: {"0x123456789...":2,"0x987654321...":10}
+   */
+  let addressList
+  switch(type) {
+    case "whitelist":
+        addressList = require('/lib/whitelistAddressList.json')
+        break;
+    case "team":
+        addressList = require('/lib/teamAddressList.json')
+        break;
+  }
   
   // Enumerated value; this should match the struct variable in your Smart Contract
   const CouponTypeEnum = {
-    Whitelist: 0
+    Whitelist: 0,
+    Team: 1
   }
 
   // HELPER FUNCTIONS
+  /**
+   * * createCoupon
+   * @param {hash} hash 
+   * @param {hex} signerPvtKey 
+   * @returns ECDSA signature of a message hash.
+   * @notice The ECDSA (Elliptic Curve Digital Signature Algorithm) is a cryptographically 
+   *   secure digital signature scheme, based on the elliptic-curve cryptography (ECC).
+   */
   function createCoupon(hash, signerPvtKey) {
     return ecsign(hash, signerPvtKey);
   }
 
+  /**
+   * * generateHashBuffer
+   * @param {array} typesArray 
+   * @param {array} valueArray 
+   * @returns a keccak256 hash
+   * @notice Keccak256 is a cryptographic function built into solidity. This function 
+   *   takes in any amount of inputs and converts it to a unique 32 byte hash.
+   */
   function generateHashBuffer(typesArray, valueArray) {
     return keccak256(
       toBuffer(ethers.utils.defaultAbiCoder.encode(typesArray,valueArray))
     )
   }
 
+  /**
+   * * serializeCoupon
+   * @param {coupon} coupon 
+   * @returns a serialized object
+   */
   function serializeCoupon(coupon) {
     return {
       r: bufferToHex(coupon.r),
@@ -42,47 +88,72 @@ export default function handler(req, res) {
     }
   }
 
-  const couponsSetup = () => {
-    if (fs.existsSync('lib/coupons')) {
-      fs.rmSync('lib/coupons', { recursive: true})
-    }
-    fs.mkdirSync('lib/coupons');
-  }
 
-  const generateWhitelistCoupons = () => {
-    let coupons = {};
-
-    couponsSetup();
-
+  /**
+   * * generateCoupons
+   * @dev This function iterates through a list of addresses, converting them
+   * to a signed hash of the coupon details and writing them out to a JSON file
+   */
+  const generateCoupons = () => {
     try {
-      for ( const [address, qty] of Object.entries(whitelistAddresses) ) {
+      // Iterate through addresses list
+      for ( const [address, qty] of Object.entries(addressList) ) {
         
-        if (ethers.utils.isAddress(address)){          
+        // Verify that the address is a valid address (many whitelist/allowlist signups include invalid addresses)
+        if (ethers.utils.isAddress(address)){
+
+          // Set userAddress to a Checksum Address of the address
+          // If address is an invalid 40-nibble HexString or if it contains mixed case 
+          //   and the checksum is invalid, an INVALID_ARGUMENT Error is thrown.
+          // The value of address may be any supported address format.
           const userAddress = ethers.utils.getAddress(address);
         
+          // Set our Coupon Type
+          let couponType
+          switch(type) {
+            case "whitelist":
+                couponType = CouponTypeEnum["Whitelist"]
+                break;
+            case "team":
+                couponType = CouponTypeEnum["Team"]
+                break;
+          }
+
+          // Call our helper function to generate the hash buffer
           const hashBuffer = generateHashBuffer(
             ["uint256", "uint256", "address"],
-            [CouponTypeEnum["Whitelist"], qty, userAddress]
+            [couponType, qty, userAddress]
           );
         
+          // Call our helper function to sign our hashed buffer and create the coupon
           const coupon = createCoupon(hashBuffer, signerPvtKey);
+
+          // Add the wallet address with allotted mints and coupon to our coupons object
           coupons[userAddress.toLowerCase()] = {
             qty,
             coupon: serializeCoupon(coupon)
           }
         } else {
+          // Kick out a log of addresses that were flagged as invalid
           console.log(address + ': Invalid Address')
         }
       }
+
+      // Convert our coupons object to a readable string
       const writeData = JSON.stringify(coupons, null, 2);
-      fs.writeFileSync('lib/coupons/devwhitelistCoupons.json', writeData);
+
+      // Write our coupons to a new whitelistCoupons.json file
+      fs.writeFileSync(`coupons/${type}Coupons.json`, writeData);
+
     } catch (err) {
-      console.log(err)
+      // Log any errors
+      console.error(err)
+      res.status(500).json({ message: err })
     }
   }
 
+  // Call our generateCoupons function
+  generateCoupons();
 
-  generateWhitelistCoupons();  
-
-  res.status(200).json({ message: 'Success!' })
+  res.status(200).json({ message: `Success! Your file has successfully been written to /coupons/${type}Coupons.json` })
 }
--- coupon-signer/pages/index.js
+++ coupon-signer/pages/index.js
@@ -1,16 +1,39 @@
-import { keccak256, toBuffer, ecsign, bufferToHex } from 'ethereumjs-util';
-import { ethers } from 'ethers';
-import Link from 'next/link';
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [message,setMessage] = useState('')
+
+  const retrieveCoupons = (type) => {
+    fetch(`/api/retrieveCoupons?type=${type}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.message)
+        setMessage(data.message)
+      })
+    
+  }
+
   return (
     <div className="text-center">
       <h1 className="text-8xl font-bold uppercase text-center">Coupon Signer</h1>
-      <Link href="/api/retrieveCoupons">
-        <a
-          className="text-white inline-block mt-8 bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-        >Retrieve Coupons</a>
-      </Link>
+      <button 
+        type="button"
+        onClick={()=>retrieveCoupons("whitelist")}
+        className="text-white inline-block mt-8 bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Retrieve Whitelist Coupons
+      </button>
+      <button 
+        type="button"
+        onClick={()=>retrieveCoupons("team")}
+        className="text-white inline-block mt-8 bg-gradient-to-r from-red-500 to-purple-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Retrieve Team Coupons
+      </button>
+      <div className="mt-20 text-center">
+        { message !== '' && message}
+      </div>
     </div>
   )
 }
