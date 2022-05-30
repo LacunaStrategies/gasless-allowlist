05042773993f6fdb1cf7c08c66ee1087856df1d7
LacunaStrategies <102769956+LacunaStrategies@users.noreply.github.com>
2022-05-30 10:30:45 -0700

    Update config.js

--- minting-dapp/src/config.js
+++ minting-dapp/src/config.js
@@ -1,4 +1,9 @@
 export const contractAddress = "0xcf7B6D27FdE602AE0B0D12D5C77A1fD67ae4B394"
-export const activeNetwork = "Rinkeby Network" // [Rinkeby, Mainnet]
-export const activeChainId = "0x4" // [0x4, 0x1]
+export const activeNetwork = {
+    name: "Rinkeby Network",
+    symbol: "ETH",
+    chainId: "0x4",
+}
+export const presalePriceInWei = 700000000000000
+export const gasLimit = 285000
 export const activePhase = "team" // [presale, team]
